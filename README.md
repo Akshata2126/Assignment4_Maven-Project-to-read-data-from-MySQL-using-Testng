d35da8181fd6d0a0c5084b9c86ae7a55230b10ee
Akshata2126 <akshatapujari@gmail.com>
2019-01-26 23:26:06 +0530

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-"# Assignment_3_Maven-project-using-TestNG" 
-"# Assignment_3_Maven-project-using-TestNG" 
--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,6 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
+import org.testng.annotations.Test;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -14,41 +11,45 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class App {
 
-		// TODO Auto-generated method stub
-		public static XSSFSheet ExcelWSheet;
-		public static XSSFWorkbook ExcelWorkbook;
-		public XSSFCell ExcelCell;
-		public static XSSFRow ExcelRow;
-		
-		public static String filename_path="C:\\Users\\Manoj\\my-app\\Employee_Data.xlsx";
-		
-		public static void main(String args[]) throws IOException
-		{
-		FileInputStream ExcelFile = new FileInputStream(filename_path);
-		ExcelWorkbook = new XSSFWorkbook(ExcelFile);
-		ExcelWSheet = ExcelWorkbook.getSheet("Sheet1");
-		int rowCount= ExcelWSheet.getLastRowNum() -ExcelWSheet.getFirstRowNum();
-	
-	
-		// Create a loop for a reading row
-		 for (int i = 0; i <=rowCount; i++) {
+//Declare variables to access ExcelFile
+public static XSSFWorkbook ExcelWorkbook;
+public static XSSFSheet ExcelWSheet;
+public XSSFCell ExcelCell;
+public static XSSFRow ExcelRow;
 
+  //Location of Excel File
+public static String filename_path="Employee_Data.xlsx";
+
+   //Testcase 1 to read data having first priority
+  @Test(priority=0)
+  public void Read_Data() throws IOException {
+	  //Create an object of FileInputStream class to read excel file
+	  FileInputStream ExcelFile = new FileInputStream(filename_path);
+	  //create object of XSSFWorkbook class
+	  ExcelWorkbook = new XSSFWorkbook(ExcelFile);
+	  //Selecting Sheet1 from Workbook
+	  ExcelWSheet = ExcelWorkbook.getSheet("Sheet1"); 
+	  
+  }
+
+  //Testcase 1 to print data having first priority
+  @Test(priority=1)
+  public void Print_Data() {
+	  //Find number of rows in excel sheet
+	  int rowCount= ExcelWSheet.getLastRowNum() -ExcelWSheet.getFirstRowNum();
+		  //Create a loop to read rows
+		 for (int i = 0; i < rowCount+1; i++) {
+			 //Read a row at i position
 			 ExcelRow = ExcelWSheet.getRow(i);
-
+			 
 		        //Create a loop to print cell values in a row
-
 		        for (int j = 0; j < ExcelRow.getLastCellNum(); j++) {
-
 		            //Print Excel data in console
-
 		            System.out.print(ExcelRow.getCell(j)+" | ");
-
-		        }
-
-		        System.out.println();
-		 }
-			
-		}
-
-	}
-
+	        }
+	        System.out.println();
+		 }  
+  }
+  
+}
+  
