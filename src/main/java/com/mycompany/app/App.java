88bfbf32499eaa0ab4aeb95fb589187383717c02
Akshata2126 <46868365+Akshata2126@users.noreply.github.com>
2019-02-03 16:06:56 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,17 +1,12 @@
 package com.mycompany.app;
 
 import org.testng.annotations.Test;
-
 import java.sql.Connection;
-
 import org.testng.annotations.BeforeMethod;
-
-
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import org.testng.annotations.AfterMethod;
 
 public class App {
@@ -26,9 +21,9 @@ public class App {
 	  try
 	 {
 	// create our mysql database connection
-    String myUrl = "jdbc:mysql://localhost/test";
-    //provide userid and password for the database
-    conn = DriverManager.getConnection(myUrl, "root", "Alwayshappy@21");
+   	 String myUrl = "jdbc:mysql://localhost/test";
+    	//provide userid and password for the database
+   	 conn = DriverManager.getConnection(myUrl, "root", "Alwayshappy@21");
 	 }
 	 catch (Exception e) {
 		// TODO: handle exception
@@ -39,16 +34,17 @@ public class App {
   public void insert_Data() {
 	  try
 	  {
-		//create statement object 
+	   //create statement object 
 	   st = conn.createStatement();
 	   //SQL insert query for Employee_Details Table
 	   String insert_query= "insert into test.Employee_Details(Emp_ID, Emp_name , Emp_City,Project_ID)"+ " values (1010,'karan','Mumbai', 'PROJ02');";
-       //execute insert query and update in database
+           //execute insert query and update in database
 	   st.executeUpdate(insert_query);
-       System.out.println("Row inserted in Employee_Details Table ");
-       String query = "SELECT * FROM test.Employee_Details;";
-      //contains result of database query
-      ResultSet rs = st.executeQuery(query);
+           System.out.println("Row inserted in Employee_Details Table ");
+	   //Query to display all rows in a table
+       	   String query = "SELECT * FROM test.Employee_Details;";
+      	   //contains result of database query
+     	   ResultSet rs = st.executeQuery(query);
       
       // iterate through the java ResultSet
       while (rs.next())
@@ -75,12 +71,12 @@ public class App {
 		  st = conn.createStatement();
 		  //SQL insert query for Project_Details Table
 		  String insert_query= "insert into test.Project_Details(Project_ID, Project_Name , Domain)"+ " values ('Proj05','BT','Support');";
-	      st.executeUpdate(insert_query);
-	      System.out.println("Row inserted in Project_Details Table ");
-	      //SQL SELECT query. 
-	      String pro_query = "SELECT * FROM test.Project_Details;";
-	      //contains result of database query
-	      ResultSet rs = st.executeQuery(pro_query);
+	      	  st.executeUpdate(insert_query);
+	          System.out.println("Row inserted in Project_Details Table ");
+	      	 //Query to display all rows in a table
+	      	  String pro_query = "SELECT * FROM test.Project_Details;";
+	      	  //contains result of database query
+	          ResultSet rs = st.executeQuery(pro_query);
 	      
 	      // iterate through the java resultset
 	      while (rs.next())
